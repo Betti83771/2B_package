c8a09d807f768a92f680458cb284f096dab4c9e2
bettiabba <akylu8@gmail.com>
2022-08-18 11:45:42 +0200

    adjustments

--- rigaprop_script.py
+++ rigaprop_script.py
@@ -1,11 +1,11 @@
 import bpy
 
-def rigaprop(collection):
+def rigaprop(context):
     """Run this script with the collection above the linking collection active in the outliner (the one that has the same name of file name)"""
 
     filename = bpy.path.basename(bpy.data.filepath).replace(".blend", "")
 
-    current_coll = collection
+    current_coll = context.collection
 
     obj_coll = next(coll for coll in current_coll.children if filename in coll.name)
 
@@ -14,7 +14,7 @@ def rigaprop(collection):
 
         if filename + coll_suffix in bpy.data.collections.keys(): continue
 
-        coll = bpy.data.collections.new(filename  + "_RIG")
+        coll = bpy.data.collections.new(filename  + coll_suffix)
 
 
         obj_coll.children.link(coll)
@@ -23,6 +23,8 @@ def rigaprop(collection):
 
     current_coll.children.link(metarig_coll)
 
-    new_rig_data = bpy.data.armatures.new(filename + "_METARIG" )
-    new_rig = bpy.data.objects.new(filename + "_METARIG" ,new_rig_data)
-    metarig_coll.objects.link(new_rig)
+    bpy.ops.object.armature_add(enter_editmode=False, align='WORLD', location=(0.00266746, -0.19931, 0.734643), scale=(1, 1, 1))
+
+
+    metarig_coll.objects.link(context.active_object)
+    current_coll.objects.unlink(context.active_object)
