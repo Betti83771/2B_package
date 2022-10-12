b163300100a5b01f6c7d7bc6f8515b3e7a40206d
Betti <akylu8@gmail.com>
2022-10-12 19:36:05 +0200

    version changed: 1.4.4

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "2 Battiti",
     "author": "Betti",
-    "version": (1, 4, 3), 
+    "version": (1, 4, 4), 
     "blender": (3, 0, 0),
     "location": "View3D > 2B",
     "description": """Addons for the 2B production""",
